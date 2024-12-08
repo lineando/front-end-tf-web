cdd46b2957dc205f175f6e99934ebc9c10ec5783
aline <165079083+lineando@users.noreply.github.com>
2024-12-08 22:12:26 +0000

    a

---
+++ confirmar.js
@@ -0,0 +1,7 @@
+function confirmarRetirada() {
+    console.log("Retirada do item registrada com sucesso!");
+  }
+function confirmarCadastro() {
+    console.log("Cadastro do item realizado com sucesso!");
+  }
+  
